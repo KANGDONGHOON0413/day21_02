bd3853fa8a90ace5554cf4a9f9cef49ad38fd8c1
KANGDONGHOON0413 <kmerits@gmail.com>
2022-07-04 09:27:22 +0900

    초기 커밋.

---
+++ day21_02/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace day21_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(222, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(191, 215);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button1 가리기";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 503);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Layout += new System.Windows.Forms.LayoutEventHandler(this.Form1_Layout);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ day21_02/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace day21_02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //방법2: Size = new Size(550,550);
+
+            //방법3: 
+            //Width = 550;
+            //Height = 550;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)   //폼의 크기변경 감지
+        {
+            Control ctrl = (Control)sender; //매개변수값을 언박싱을 한 후 넘겨준다.
+
+            //폼의 크기 변경이 감지되었을 때 크기를 다시금 재조정하는 부분
+            ctrl.Width = 550;
+            ctrl.Height = 550;
+
+            MessageBox.Show("resize detected");
+
+            // 간소화
+            //Width = 550;
+            //Height = 550;
+        }
+
+        private void Form1_Layout(object sender, LayoutEventArgs e) //폼의크기변경 + 폼의 컨트롤 배치가 바뀌는 경우
+        {
+            MessageBox.Show("layout change detected");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Hide();
+        }
+    }
+}
